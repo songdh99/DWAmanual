d88526ad6e467bbae4f7d950c4694ea1eb34a3fd
songdh99 <94097689+songdh99@users.noreply.github.com>
2022-08-01 23:33:08 +0900

    Add files via upload

--- test_song.py
+++ test_song.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from multiprocessing import current_process
 import turtle
 import numpy as np
 import rospy
@@ -12,6 +13,13 @@ mps_n = 2
 rps_n = 13
 mps = [0.15, 0.10]
 radps = [0, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, -0.3, -0.5, -0.6, -0.7, -0.8, -0.9]
+goal_stop_x = 1.2225
+goal_stop_y = -0.006
+goal_distance = 0
+current_speed = 0
+turn_speed = 0
+angle_of_turn = 0
+
 
 angle_160 = np.array(-80, 80).reshape(160,1,1,1)
 
@@ -38,6 +46,17 @@ class SelfDrive:
 
     
 
-def directory_fun():
+def obstacle_collision_fun():
+    global goal_distance
+    global current_speed
+    global turn_speed
+    global angle_of_turn
+    if turn_speed == 0:
+        return goal_distance / current_speed
+    else:
+        return angle_of_turn / turn_speed
+def time_to_stop():
+    
 
-                
+
+    
